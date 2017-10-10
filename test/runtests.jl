229e4d539c80923d346ea45b17177865be4c3856
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2017-10-10 20:25:23 +0200

    Add basic tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,15 @@
 using OptimTestProblems
 using Base.Test
 
-@test true
+uvp = OptimTestProblems.UnivariateProblems.examples
+for (name, p) in uvp
+    for (miz, mia) in zip(p.minimizers, p.minima)
+        @test p.f(miz) ≈ mia
+    end
+end
+mvp = OptimTestProblems.UnconstrainedProblems.examples
+for (name, p) in mvp
+    gs = similar(p.initial_x)
+    p.g!(gs, p.solutions)
+    @test sum(gs) - zero(eltype(gs)) < 1e-32
+end

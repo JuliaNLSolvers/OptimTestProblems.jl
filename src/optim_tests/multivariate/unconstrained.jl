b25209258fec52df4cc16629cd1dcf2138f17569
Asbjørn Nilsen Riseth <anriseth@gmail.com>
2017-11-07 17:17:19 +0000

    Allow parameters in OptimizationProblem

--- src/optim_tests/multivariate/unconstrained.jl
+++ src/optim_tests/multivariate/unconstrained.jl
@@ -6,8 +6,18 @@ module UnconstrainedProblems
 ### Link: www.researchgate.net/profile/Montaz_Ali/publication/226654862_A_Numerical_Evaluation_of_Several_Stochastic_Algorithms_on_Selected_Continuous_Global_Optimization_Test_Problems/links/00b4952bef133a1a6b000000.pdf
 ###
 ### [2] Fletcher & Powell: A rapidly convergent descent method for minimization,
+###
+### [3] Moré JJ, Garbow BS, Hillstrom KE: Testing unconstrained optimization software. ACM T Math Software. 1981
+###
+###
+import Base.gradient
 
-immutable OptimizationProblem
+export OptimizationProblem, objective, gradient, hessian
+
+#######################
+# TODO: We could write fg! calls as well here
+#######################
+immutable OptimizationProblem{P}
     name::AbstractString
     f::Function
     g!::Function
@@ -16,7 +26,29 @@ immutable OptimizationProblem
     solutions::Vector
     isdifferentiable::Bool
     istwicedifferentiable::Bool
+    parameters::P
 end
+
+OptimizationProblem(name::AbstractString,
+                    f::Function,
+                    g!::Function,
+                    h!::Function,
+                    initial_x::Vector{Float64},
+                    solutions::Vector,
+                    isdifferentiable::Bool,
+                    istwicedifferentiable::Bool) =
+                        OptimizationProblem(name, f, g!, h!, initial_x,
+                                            solutions, isdifferentiable,
+                                            istwicedifferentiable,
+                                            nothing)
+
+objective(p::OptimizationProblem{P}) where P<:Void = p.f
+gradient(p::OptimizationProblem{P}) where P<:Void = p.g!
+hessian(p::OptimizationProblem{P}) where P<:Void = p.h!
+
+objective(p::OptimizationProblem{P}) where P = x-> p.f(x,p.parameters)
+gradient(p::OptimizationProblem{P}) where P = (out,x)-> p.g!(out,x,p.parameters)
+hessian(p::OptimizationProblem{P}) where P = (out,x)-> p.h!(out,x,p.parameters)
 
 examples = Dict{AbstractString, OptimizationProblem}()
 

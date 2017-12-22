1aa63510bcffbb780cea125c10fb8806a9c80cb9
Asbjørn Nilsen Riseth <anriseth@gmail.com>
2017-12-22 09:44:06 +0000

    Add eight more test problems/families (#2)
    * Allow parameters in OptimizationProblem
    
    * Add test problems. Factorize examples.
    
    * Store minimum value in problem (useful if you don't want to store the solution)
    
    * Restore outer random number seed

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,9 +7,30 @@ for (name, p) in uvp
         @test p.f(miz) ≈ mia
     end
 end
+
+using OptimTestProblems.UnconstrainedProblems
 mvp = OptimTestProblems.UnconstrainedProblems.examples
 for (name, p) in mvp
+    soltest = !any(isnan, p.solutions)
+
+    if startswith(name, "Penalty Function I")
+        # The provided solutions are not exact
+        tol = 1e-16
+    else
+        tol = 1e-32
+    end
+
+    f = objective(p)
+    soltest && @test f(p.solutions) ≈ p.minimum
+
     gs = similar(p.initial_x)
-    p.g!(gs, p.solutions)
-    @test sum(gs) - zero(eltype(gs)) < 1e-32
+    g! = gradient(p)
+    g!(gs, p.solutions)
+    soltest && @test norm(gs, Inf) < tol
+
+    fg! = objective_gradient(p)
+    fgs = similar(gs)
+    g!(gs, p.initial_x)
+    @test fg!(fgs, p.initial_x) ≈ f(p.initial_x)
+    @test norm(fgs.-gs, Inf)  < eps(eltype(gs))
 end

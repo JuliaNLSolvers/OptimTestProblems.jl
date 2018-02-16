92d156da1b8a940f8de3ea1c8cf0026d61998b05
Asbjørn Nilsen Riseth <anriseth@gmail.com>
2018-02-16 05:46:23 +0100

    Don't break backwards compatibility

--- src/OptimTestProblems.jl
+++ src/OptimTestProblems.jl
@@ -2,8 +2,11 @@ module OptimTestProblems
 
 export MultivariateProblems, UnivariateProblems
 
-
 include("optim_tests/multivariate/multivariate.jl")
 include("optim_tests/univariate/bounded.jl")
+
+# Deprecation stuff
+UnconstrainedProblems = OptimTestProblems.MultivariateProblems.UnconstrainedProblems
+export UnconstrainedProblems
 
 end # module
